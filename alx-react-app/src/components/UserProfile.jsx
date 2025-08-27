97db315f29c8b300f9ceee7f223eec00fe8cb14d
philemon <philemonmaosa@gmail.com>
2025-08-27 12:08:43 +0300

    I have edited the file

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 
-function UserProfile({name,age,bio}) {
+function UserProfile(props) {
 
 return(
 <div>
 
-<h2>{name}</h2>
-<p>Age:{age} </p>
-<p>Bio:{bio} </p>
+<h2>{props.name}</h2>
+<p>Age:{props.age} </p>
+<p>Bio:{props.bio} </p>
 
 </div>
 );
