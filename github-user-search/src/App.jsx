9d45224d0859f861f68eedcd7c96262ecb6a3139
philemon <philemonmaosa@gmail.com>
2025-09-17 15:53:11 +0300

    Added jsx extension to the files import

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./components/Home";
-import Suggestions from "./components/Suggestions";
-import NotFound from "./components/NotFound";
+import Header from "./components/Header.jsx";
+import Home from "./components/Home.jsx";
+import Suggestions from "./components/Suggestions.jsx";
+import NotFound from "./components/NotFound.jsx";
 
 function App() {
   return (
@@ -11,6 +11,10 @@ function App() {
       <div style={{ maxWidth: 900, margin: "0 auto", padding: 20 }}>
         <Header />
         <main style={{ marginTop: 20 }}>
+<h1 className="text-3xl font-bold text-blue-600">
+  Tailwind is working! 🎉
+</h1>
+
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/suggestions" element={<Suggestions />} />
