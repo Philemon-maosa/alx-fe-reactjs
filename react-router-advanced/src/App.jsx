b7932293c6e471fc53e74242e0c4f93c60c50562
philemon <philemonmaosa@gmail.com>
2025-10-06 11:19:22 +0300

    Added routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -5,18 +5,21 @@ import "./App.css";
 import Profile from "./components/Profile";
 import ProfileDetails from "./components/ProfileDetails";
 import ProfileSettings from "./components/ProfileSettings";
+import BlogPost from "./components/BlogPost"; // ✅ Add this import
 
 function Home() {
   return (
     <div className="p-6 text-center">
       <h1 className="text-3xl font-bold mb-4">Welcome to the App</h1>
-      <p>Click below to visit a profile (dynamic route)</p>
-      <Link
-        to="/profile/1"
-        className="text-blue-600 hover:underline mt-4 inline-block"
-      >
-        Go to Profile #1
-      </Link>
+      <p>Click below to visit a profile or a blog (dynamic routes)</p>
+      <div className="flex flex-col gap-2 mt-4">
+        <Link to="/profile/1" className="text-blue-600 hover:underline">
+          Go to Profile #1
+        </Link>
+        <Link to="/blog/101" className="text-blue-600 hover:underline">
+          Read Blog Post #101
+        </Link>
+      </div>
     </div>
   );
 }
@@ -31,6 +34,9 @@ function App() {
         <Link to="/profile/1" className="hover:text-blue-300">
           Profile
         </Link>
+        <Link to="/blog/101" className="hover:text-blue-300">
+          Blog
+        </Link>
       </nav>
 
       <Routes>
@@ -38,10 +44,12 @@ function App() {
 
         {/* Dynamic route: /profile/:id */}
         <Route path="/profile/:id" element={<Profile />}>
-          {/* Nested routes inside Profile */}
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
+
+        {/* ✅ Dynamic route: /blog/:id */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
