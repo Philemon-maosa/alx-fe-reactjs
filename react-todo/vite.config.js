b11a8f143f4aa70535fee612c6107761077b60d6
philemon <philemonmaosa@gmail.com>
2025-10-06 13:09:30 +0300

    edited cite config file

--- react-todo/vite.config.js
+++ react-todo/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    transformMode: {
+      web: [/\.[jt]sx$/], // ✅ ensures JSX files are properly transformed
+    },
+  },
 })
